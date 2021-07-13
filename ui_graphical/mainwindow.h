6be642d10c9dc35dc046643f52560e2621c444e2
Sergei Udris <sergei.udris@gmail.com>
2021-07-13 16:58:57 +0300

    Revert "Revert "remove license, attribution headers in files, programmer name - repo is code only""
    This reverts commit 5e696c0cadc5f75e4e7b90fc2c0a94b64fea2207.

--- ui_graphical/mainwindow.h
+++ ui_graphical/mainwindow.h
@@ -1,27 +1,3 @@
-/****************************************************************************
-**
-** Copyright (C) 2015 sergeiudris
-** Contact: sergei.udris@gmail.com
-**
-** This file is part of Linux Memory Monitor
-** Program uses /proc interface to calculate precise RAM usage for a group of processes,
-** e.g. browser,database etc., excluding shared and not present(swapped out) memory pages
-**
-**
-** Linux Memory Monitor is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-
-** Linux Memory Monitor is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-
-** You should have received a copy of the GNU General Public License
-** along with Linux Memory Monitor.  If not, see <http://www.gnu.org/licenses/>.
-**
-****************************************************************************/
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
